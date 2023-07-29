d10604cc5e09e8d0b65f728f0c08c91214250999
JM <jacobmontpetit@gmail.com>
2023-07-29 00:15:33 -0400

    src: add int tests

--- src/ziggy-pack-test.zig
+++ src/ziggy-pack-test.zig
@@ -68,3 +68,75 @@ test "float 64  0.128129 pack" {
     pack.pack(&buffer, &i, packtest);
     try testing.expectEqualSlices(u8, &expectation, &buffer);
 }
+
+test "Int 8 -95 pack" {
+    const packtest: i8 = -95;
+    const expectation = [_]u8{ 0xdc, 0xa1 };
+    var buffer = [_]u8{0} ** pack.packSize(packtest);
+    var i: usize = 0;
+    pack.pack(&buffer, &i, packtest);
+    try testing.expectEqualSlices(u8, &expectation, &buffer);
+}
+
+test "Int 8 95 pack" {
+    const packtest: i8 = 95;
+    const expectation = [_]u8{ 0xdc, 95 };
+    var buffer = [_]u8{0} ** pack.packSize(packtest);
+    var i: usize = 0;
+    pack.pack(&buffer, &i, packtest);
+    try testing.expectEqualSlices(u8, &expectation, &buffer);
+}
+
+test "Int 16 Max Int pack" {
+    const packtest: i16 = std.math.maxInt(i16);
+    const expectation = [_]u8{ 0xdd, 0x7f, 0xff };
+    var buffer = [_]u8{0} ** pack.packSize(packtest);
+    var i: usize = 0;
+    pack.pack(&buffer, &i, packtest);
+    try testing.expectEqualSlices(u8, &expectation, &buffer);
+}
+
+test "Int 16 Min Int pack" {
+    const packtest: i16 = std.math.minInt(i16);
+    const expectation = [_]u8{ 0xdd, 0x80, 0x0 };
+    var buffer = [_]u8{0} ** pack.packSize(packtest);
+    var i: usize = 0;
+    pack.pack(&buffer, &i, packtest);
+    try testing.expectEqualSlices(u8, &expectation, &buffer);
+}
+
+test "Int 32 Max Int pack" {
+    const packtest: i32 = std.math.maxInt(i32);
+    const expectation = [_]u8{ 0xde, 0x7f, 0xff, 0xff, 0xff };
+    var buffer = [_]u8{0} ** pack.packSize(packtest);
+    var i: usize = 0;
+    pack.pack(&buffer, &i, packtest);
+    try testing.expectEqualSlices(u8, &expectation, &buffer);
+}
+
+test "Int 32 Min Int pack" {
+    const packtest: i32 = std.math.minInt(i32);
+    const expectation = [_]u8{ 0xde, 0x80, 0, 0, 0 };
+    var buffer = [_]u8{0} ** pack.packSize(packtest);
+    var i: usize = 0;
+    pack.pack(&buffer, &i, packtest);
+    try testing.expectEqualSlices(u8, &expectation, &buffer);
+}
+
+test "Int 64 Max Int pack" {
+    const packtest: i64 = std.math.maxInt(i64);
+    const expectation = [_]u8{ 0xdf, 0x7f, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff };
+    var buffer = [_]u8{0} ** pack.packSize(packtest);
+    var i: usize = 0;
+    pack.pack(&buffer, &i, packtest);
+    try testing.expectEqualSlices(u8, &expectation, &buffer);
+}
+
+test "Int 64 Min Int pack" {
+    const packtest: i64 = std.math.minInt(i64);
+    const expectation = [_]u8{ 0xdf, 0x80, 0, 0, 0, 0, 0, 0, 0 };
+    var buffer = [_]u8{0} ** pack.packSize(packtest);
+    var i: usize = 0;
+    pack.pack(&buffer, &i, packtest);
+    try testing.expectEqualSlices(u8, &expectation, &buffer);
+}
